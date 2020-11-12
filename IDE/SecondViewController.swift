ef6716c588ec542cd8925cf5b2a5fe3e2a5e4696
Eric Brito <eric_brito@msn.com>
2020-11-12 13:02:29 -0300

    - Commit inicial

--- IDE/SecondViewController.swift
+++ IDE/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  IDE
 //
 //  Created by Eric Alves Brito on 09/11/20.
@@ -8,31 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Tela 1: viewDidLoad")
+        print("Tela 2: viewDidLoad")
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("Tela 1: viewWillAppear")
+        print("Tela 2: viewWillAppear")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("Tela 1: viewDidAppear")
+        print("Tela 2: viewDidAppear")
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print("Tela 1: viewWillDisappear")
+        print("Tela 2: viewWillDisappear")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("Tela 1: viewDidDisappear")
+        print("Tela 2: viewDidDisappear")
     }
 }
-
